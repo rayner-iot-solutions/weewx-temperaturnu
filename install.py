406edeaf29b1ba7d990a7e543b69276a1b8b7023
RC Chuah <44928288+rc-chuah@users.noreply.github.com>
2026-03-28 01:27:31 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
-# installer for temperatur.nu
+# installer for temperaturnu
 # Copyright 2026 RC Chuah (Based on weewx-windy by Matthew Wall and Jacques Terrettaz)
 # Distributed under the terms of the GNU Public License (GPLv3)
 
